9eb837bf155c041118827ac495c7e246366a4ebc
hulinNeil <1289739946@qq.com>
2018-04-03 11:31:25 +0800

    '增加对margin-right:auto;等的过滤；修改input类型tel为text'

--- src/weapp/transform/helper/style/declaration/margin/index.js
+++ src/weapp/transform/helper/style/declaration/margin/index.js
@@ -1,12 +1,31 @@
+function setAutovalue (value) {
+  if (~value.indexOf('auto')) {
+    // TODO 待处理
+    return 'I:'
+  }
+  return ''
+}
+
 export default {
+//   'margin': (value, declaration, addDeclaration) => {
+//     if (~value.indexOf('auto')) {
+//       // TODO 待处理
+//       return 'I:'
+//     }
+//   },
   'margin': (value, declaration, addDeclaration) => {
-    if (~value.indexOf('auto')) {
-      // TODO 待处理
-      return 'I:'
-    }
+    return setAutovalue(value)
   },
-  'margin-bottom': '',
-  'margin-left': '',
-  'margin-right': '',
-  'margin-top': ''
+  'margin-bottom': (value, declaration, addDeclaration) => {
+    return setAutovalue(value)
+  },
+  'margin-left': (value, declaration, addDeclaration) => {
+    return setAutovalue(value)
+  },
+  'margin-right': (value, declaration, addDeclaration) => {
+    return setAutovalue(value)
+  },
+  'margin-top': (value, declaration, addDeclaration) => {
+    return setAutovalue(value)
+  }
 }
